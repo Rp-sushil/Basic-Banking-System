f3dd071b40f57c8285f14e0b66028010d841f129
Rp-sushil <sushil.18je0861@am.iitism.ac.in>
2021-01-04 07:32:22 +0530

    fix interner server error

--- index.js
+++ index.js
@@ -27,13 +27,6 @@ client.connect((err) => {
 // id_counter
 var count = 16;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
-
 // Routes
 app.get("/customers", async (req, res) => {
   try {
